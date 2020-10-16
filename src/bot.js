bc4fa5e2a0e20a19561d8c2293f1701b83654114
ThatRandomJar <thatrandomjar@gmail.com>
2020-10-16 20:00:46 +0530

    master

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,20 @@
 require("dotenv").config();
 
-const { Client } = require("discord.js");
+const { Client, Message } = require("discord.js");
 const client = new Client();
 
-client.login(process.env.DISCORDJS_BOT_TOKEN)
+const PREFIX = ".";
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (msg) => {
+  if (msg.author.bot) return;
+    if (msg.content === 'UwU') {
+      msg.channel.send('OwO');
+    }
+
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
